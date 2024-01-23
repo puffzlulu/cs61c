efd49a29896b432a550a55b9350e36757a0dc935
puff <puffzlulu@gmail.com>
2024-01-23 17:46:34 +0800

    complete lab02

---
+++ fa22-lab-starter-main/lab02/ll_cycle.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include "ll_cycle.h"
+
+int ll_has_cycle(node *head) {
+   node *tortoise = head;
+   node *hare = head;                                                                 
+   while (hare != NULL) {
+       tortoise = tortoise->next;
+       if(hare->next==NULL) return 0;
+       hare = hare->next->next;
+       if (tortoise == hare) {
+           return 1;
+       }
+   }
+   return 0;
+}
